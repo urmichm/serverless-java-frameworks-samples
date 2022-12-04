09077b923d60989ba2f5a09bae3babff3089be33
Michael <urmich.m@gmail.com>
2022-12-04 11:24:48 +0100

    only me only native only my class as handler part II

--- springboot-mvc/src/main/java/software/amazonaws/example/product/product/SpringBootSampleApplication.java
+++ springboot-mvc/src/main/java/software/amazonaws/example/product/product/SpringBootSampleApplication.java
@@ -3,32 +3,32 @@
 
 package software.amazonaws.example.product.product;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.xray.entities.TraceHeader;
-import com.amazonaws.xray.entities.TraceID;
-import com.amazonaws.xray.interceptors.TracingInterceptor;
-import org.joda.time.DateTime;
+//import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+//import com.amazonaws.xray.entities.TraceHeader;
+//import com.amazonaws.xray.entities.TraceID;
+//import com.amazonaws.xray.interceptors.TracingInterceptor;
+//import org.joda.time.DateTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.TypeHint;
+//import org.springframework.nativex.hint.TypeHint;
+//
+//import java.util.HashSet;
 
-import java.util.HashSet;
-
+//@TypeHint(types = {
+//  DateTime.class,
+//  APIGatewayProxyRequestEvent.class,
+//  TracingInterceptor.class,
+//  HashSet.class,
+//  TraceHeader.class,
+//  TraceID.class
+//},
+//  typeNames = {
+//    "com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent$ProxyRequestContext",
+//    "com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent$RequestIdentity",
+//    "com.amazonaws.xray.entities.TraceHeader$SampleDecision"
+//  })
 @SpringBootApplication
-@TypeHint(types = {
-  DateTime.class,
-  APIGatewayProxyRequestEvent.class,
-  TracingInterceptor.class,
-  HashSet.class,
-  TraceHeader.class,
-  TraceID.class
-},
-  typeNames = {
-    "com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent$ProxyRequestContext",
-    "com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent$RequestIdentity",
-    "com.amazonaws.xray.entities.TraceHeader$SampleDecision"
-  })
 public class SpringBootSampleApplication {
 
   public static void main(String[] args) {
